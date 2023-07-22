0fb2a68721f12eaea87c44b819aacabf450d495a
steffenaxer <26229392+steffenaxer@users.noreply.github.com>
2023-07-22 08:24:25 +0200

    Minor refactoring

--- contribs/dvrp/src/main/java/org/matsim/contrib/zone/skims/TimeDependentTravelTimeMatrix.java
+++ contribs/dvrp/src/main/java/org/matsim/contrib/zone/skims/TimeDependentTravelTimeMatrix.java
@@ -29,8 +29,6 @@ import org.matsim.contrib.dvrp.trafficmonitoring.QSimFreeSpeedTravelTime;
 import org.matsim.contrib.zone.SquareGridSystem;
 import org.matsim.contrib.zone.ZonalSystems;
 import org.matsim.contrib.zone.Zone;
-import org.matsim.core.controler.events.IterationStartsEvent;
-import org.matsim.core.controler.listener.IterationStartsListener;
 import org.matsim.core.router.util.TravelTime;
 
 import java.util.Map;
@@ -42,17 +40,16 @@ import java.util.stream.IntStream;
  */
 public class TimeDependentTravelTimeMatrix implements TravelTimeMatrix {
 	public static final Logger LOG = LogManager.getLogger(TimeDependentTravelTimeMatrix.class);
-	private static double TIME_INTERVAL = 3600.;
-	private static double MAX_TIME = 30 * 3600.;
-
+	private static final double TIME_INTERVAL = 3600.;
+	private static final double MAX_TIME = 30 * 3600.;
 	private final SquareGridSystem gridSystem;
 	private final Map<Zone, Node> centralNodes;
 	private final TravelTimeMatrices.RoutingParams routingParams;
 	private final TimeAsTravelDisutility travelDisutility;
 	private final DvrpTravelTimeMatrixParams params;
 	private final ForkJoinPool forkJoinPool = new ForkJoinPool(4);
-	private Matrix[] freeSpeedTravelTimeMatrix;
-	private SparseMatrix[] freeSpeedTravelTimeSparseMatrix;
+	private Matrix[] travelTimeMatrix;
+	private SparseMatrix[] sparseTravelTimeMatrix;
 
 	public TimeDependentTravelTimeMatrix(Network dvrpNetwork, DvrpTravelTimeMatrixParams params, int numberOfThreads, TravelTime travelTime) {
 		this.params = params;
@@ -60,12 +57,7 @@ public class TimeDependentTravelTimeMatrix implements TravelTimeMatrix {
 		this.centralNodes = ZonalSystems.computeMostCentralNodes(dvrpNetwork.getNodes().values(), gridSystem);
 		this.travelDisutility = new TimeAsTravelDisutility(travelTime);
 		this.routingParams = new TravelTimeMatrices.RoutingParams(dvrpNetwork, travelTime, travelDisutility, numberOfThreads);
-		this.initialzeMatrix(routingParams, travelDisutility, centralNodes, params);
-	}
-
-	public static TimeDependentTravelTimeMatrix createFreeSpeedMatrix(Network dvrpNetwork, DvrpTravelTimeMatrixParams params, int numberOfThreads,
-																	  double qSimTimeStepSize) {
-		return new TimeDependentTravelTimeMatrix(dvrpNetwork, params, numberOfThreads, new QSimFreeSpeedTravelTime(qSimTimeStepSize));
+		this.initializeMatrix(routingParams, travelDisutility, centralNodes, params);
 	}
 
 	public static TimeDependentTravelTimeMatrix createMatrix(Network dvrpNetwork, DvrpTravelTimeMatrixParams params, int numberOfThreads,
@@ -73,18 +65,18 @@ public class TimeDependentTravelTimeMatrix implements TravelTimeMatrix {
 		return new TimeDependentTravelTimeMatrix(dvrpNetwork, params, numberOfThreads, new QSimFreeSpeedTravelTime(qSimTimeStepSize));
 	}
 
-	void initialzeMatrix(TravelTimeMatrices.RoutingParams routingParams, TimeAsTravelDisutility timeAsTravelDisutility, Map<Zone, Node> centralNodes, DvrpTravelTimeMatrixParams params) {
+	void initializeMatrix(TravelTimeMatrices.RoutingParams routingParams, TimeAsTravelDisutility timeAsTravelDisutility, Map<Zone, Node> centralNodes, DvrpTravelTimeMatrixParams params) {
 		int timeBins = (int) (MAX_TIME / TIME_INTERVAL);
-		this.freeSpeedTravelTimeMatrix = new Matrix[timeBins];
-		this.freeSpeedTravelTimeSparseMatrix = new SparseMatrix[timeBins];
+		this.travelTimeMatrix = new Matrix[timeBins];
+		this.sparseTravelTimeMatrix = new SparseMatrix[timeBins];
 
 		forkJoinPool.submit(() -> IntStream.range(0, timeBins).forEach(i -> {
 			LOG.info("Start matrix calculation for departure time {}", i * TIME_INTERVAL);
 			long start = System.currentTimeMillis();
 
 			double departureTime = i * TIME_INTERVAL;
-			freeSpeedTravelTimeMatrix[i] = TravelTimeMatrices.calculateTravelTimeMatrix(routingParams, centralNodes, departureTime);
-			freeSpeedTravelTimeSparseMatrix[i] = TravelTimeMatrices.calculateTravelTimeSparseMatrix(routingParams, params.maxNeighborDistance,
+			travelTimeMatrix[i] = TravelTimeMatrices.calculateTravelTimeMatrix(routingParams, centralNodes, departureTime);
+			sparseTravelTimeMatrix[i] = TravelTimeMatrices.calculateTravelTimeSparseMatrix(routingParams, params.maxNeighborDistance,
 					params.maxNeighborTravelTime, departureTime);
 
 			long end = System.currentTimeMillis();
@@ -99,19 +91,19 @@ public class TimeDependentTravelTimeMatrix implements TravelTimeMatrix {
 		if (fromNode == toNode) {
 			return 0;
 		}
-		int time = freeSpeedTravelTimeSparseMatrix[bin].get(fromNode, toNode);
+		int time = sparseTravelTimeMatrix[bin].get(fromNode, toNode);
 		if (time >= 0) {// value is present
 			return time;
 		}
-		return freeSpeedTravelTimeMatrix[bin].get(gridSystem.getZone(fromNode), gridSystem.getZone(toNode));
+		return travelTimeMatrix[bin].get(gridSystem.getZone(fromNode), gridSystem.getZone(toNode));
 	}
 
 	public int getZonalTravelTime(Node fromNode, Node toNode, double departureTime) {
 		int bin = (int) (departureTime / TIME_INTERVAL);
-		return freeSpeedTravelTimeMatrix[bin].get(gridSystem.getZone(fromNode), gridSystem.getZone(toNode));
+		return travelTimeMatrix[bin].get(gridSystem.getZone(fromNode), gridSystem.getZone(toNode));
 	}
 
 	public void update() {
-		initialzeMatrix(routingParams, travelDisutility, centralNodes, params);
+		initializeMatrix(routingParams, travelDisutility, centralNodes, params);
 	}
 }
