7e5b5e2e3a00a4d435ad939bf76cbbed8e1bc9e6
steffenaxer <26229392+steffenaxer@users.noreply.github.com>
2024-02-27 07:44:42 +0100

    Avro test

--- matsim/src/main/java/org/matsim/core/events/algorithms/EventWriterParquet.java
+++ matsim/src/main/java/org/matsim/core/events/algorithms/EventWriterParquet.java
@@ -8,6 +8,8 @@ import org.apache.parquet.avro.AvroParquetWriter;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.matsim.api.core.v01.events.Event;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.handler.BasicEventHandler;
 
 import java.io.IOException;
@@ -41,8 +43,12 @@ public class EventWriterParquet implements EventWriter, BasicEventHandler {
 		}
 	}
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		EventsManager manager = EventsUtils.createEventsManager();
+		manager.addHandler(new EventWriterParquet("test.avro"));
+		manager.initProcessing();
+		EventsUtils.readEvents(manager,"/Users/steffenaxer/Downloads/004.output_events.xml.gz");
+		manager.finishProcessing();
 	}
 
 	ParquetWriter<GenericData.Record> getWriter(Path filePath) throws IOException {
@@ -57,7 +63,7 @@ public class EventWriterParquet implements EventWriter, BasicEventHandler {
 	}
 
 	private static GenericData.Record getRecord(Event event) {
-		GenericData.Record record = new GenericData.Record(Schema.create(Schema.Type.RECORD));
+		GenericData.Record record = new GenericData.Record(Schema.create(Schema.Type.STRING));
 		record.put("time",event.getTime());
 		record.put("type",event.getEventType());
 		event.getAttributes().forEach(record::put);
