a65278ed44bfd0b211784885bca7600c9956f6d2
steffenaxer <26229392+steffenaxer@users.noreply.github.com>
2025-07-11 14:26:21 +0200

    Add license header

--- contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/insertion/parallel/DrtParallelInserterParams.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/insertion/parallel/DrtParallelInserterParams.java
@@ -1,9 +1,30 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2025 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.contrib.drt.optimizer.insertion.parallel;
 
 import jakarta.validation.constraints.NotNull;
 import org.matsim.core.config.ReflectiveConfigGroup;
 
-
+/**
+ * @author Steffen Axer
+ */
 public class DrtParallelInserterParams extends ReflectiveConfigGroup {
 
 	public DrtParallelInserterParams() {
--- contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/insertion/parallel/ParallelRequestInserterModule.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/insertion/parallel/ParallelRequestInserterModule.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2025 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.contrib.drt.optimizer.insertion.parallel;
 
 import org.matsim.contrib.drt.optimizer.DrtRequestInsertionRetryQueue;
@@ -27,7 +46,9 @@ import java.util.Optional;
 
 import static org.matsim.contrib.drt.optimizer.insertion.parallel.partitioner.requests.LoadAwareRoundRobinRequestsPartitioner.getDefaultPartitionScalingFunction;
 
-
+/**
+ * @author Steffen Axer
+ */
 public class ParallelRequestInserterModule extends AbstractDvrpModeQSimModule {
 	DrtConfigGroup drtConfigGroup;
 
